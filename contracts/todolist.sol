5fdda8410b0a2625c5d169180d36c869f8a1a7cd
HarveyBC <spk23556@gmail.com>
2025-05-17 00:29:02 -0700

    訊息

--- contracts/todolist.sol
+++ contracts/todolist.sol
@@ -1,5 +1,21 @@
 pragma solidity ^0.5.0;
 
 contract todolist {
-    uint public taskCount = 0;
+    uint public count = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping (uint => Task) public tasks;
+
+    constructor() public {
+        createTask("hello world");
+    }
+
+    function createTask(string memory _content) public {
+        count++;
+        tasks[count]= Task(count, _content, false);
+    }
 }
